f4510523fafa0705584ed58a9615f399337becaf
Nature_Day <1004934782@qq.com>
2019-07-04 09:53:34 +0800

    update project

--- lib/helloWorld.dart
+++ lib/helloWorld.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new Center(
-      child: new Text('Hello World!', textDirection: TextDirection.ltr)));
+  runApp(
+    new Center(
+      child: new Text(
+        'Hello World!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
 }
