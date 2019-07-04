38a7fb5aef68d179913bc7c20f949998d4eb44e5
Nature_Day <1004934782@qq.com>
2019-07-04 10:53:45 +0800

    update project

--- lib/helloWorld.dart
+++ lib/helloWorld.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+/**
+ *  hello world demo
+ */
 void main() {
   runApp(
-    new Center(
-      child: new Text(
-        'Hello World!',
-        textDirection: TextDirection.ltr,
+    new Container(
+      decoration: new BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: new Center(
+        child: new Text(
+          'Hello World!',
+          textDirection: TextDirection.ltr,
+        ),
       ),
     ),
   );
