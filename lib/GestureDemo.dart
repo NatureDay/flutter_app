883e378d73aac6ced22f7ad6de324ae19504b2cb
Nature_Day <1004934782@qq.com>
2019-07-04 11:03:37 +0800

    update project

---
+++ lib/GestureDemo.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+/**
+ * Gesture 的使用 demo
+ */
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'Gesture demo',
+      home: new MyButton(),
+    ),
+  );
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: new Container(
+        height: 36.0,
+        width: 300.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: new Center(
+          child: new Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
