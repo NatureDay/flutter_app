2b489d970f6aa8899ba44b0cc001a29a0c439848
Nature_Day <1004934782@qq.com>
2019-07-08 15:46:58 +0800

    update project

---
+++ lib/http_demo.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: 'http demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'http demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _doGet() {
+    print("-------_doGet-----------");
+  }
+
+  void _doPost() {
+    print("-------_doPost-----------");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Container(
+              padding: const EdgeInsets.all(16),
+              child: new GestureDetector(
+                child: new Text(
+                  'GET',
+                  style: new TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                onTap: _doGet,
+              ),
+            ),
+            new Container(
+                padding: const EdgeInsets.all(16),
+                child: new GestureDetector(
+                  child: new Text(
+                    'POST',
+                    style: new TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  onTap: _doPost,
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
