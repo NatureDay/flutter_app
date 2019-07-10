bce73e236dd3b02cc36d5af811259efbe9f5816c
Nature_Day <1004934782@qq.com>
2019-07-10 16:24:17 +0800

    update project

--- lib/app/page/logo.dart
+++ lib/app/page/logo.dart
@@ -4,25 +4,27 @@ class LogoPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return null;
+    return new LogoPageState();
   }
 }
 
 class LogoPageState extends State<LogoPage> {
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return new Scaffold(
+      body: new Container(
+        width: double.infinity,
+        height: double.infinity,
+        child:
+            new Image.asset('assets/images/bg_logo.png', fit: BoxFit.contain),
+      ),
+    );
   }
 
   @override
-  void initState() {
-
-  }
+  void initState() {}
 
   @override
-  void dispose() {
-
-  }
+  void dispose() {}
 }
