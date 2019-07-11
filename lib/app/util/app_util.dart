b294e5778e55804674dc9bb0c226914a87ea2e6e
Nature_Day <1004934782@qq.com>
2019-07-11 11:31:03 +0800

    update project

--- lib/app/util/app_util.dart
+++ lib/app/util/app_util.dart
@@ -1,10 +1,18 @@
+import 'package:flutter/material.dart';
+
 import 'package:flutter_app/app/util/shared_preferences.dart';
 
 class AppUtil {
-
   /// 判断应用是否已登录
   static bool isAppLogin() {
     return false;
   }
+}
 
+class CommonUtil {
+  /// 判断应用是否已登录
+  static void startPage(BuildContext context, String routeName,
+      {Object arguments}) {
+    Navigator.pushNamed(context, routeName, arguments: arguments);
+  }
 }
